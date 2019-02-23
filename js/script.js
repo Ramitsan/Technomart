cabc6c1dbe332bda823a47136fae18cd9cbef306
Ramitsan <ramitsan@gmail.com>
2019-02-24 01:03:46 +0300

    Модальные окна, JavaScript и многое другое.
    Модальные окна, JavaScript и многое другое.

---
+++ js/script.js
@@ -0,0 +1,85 @@
+var link = document.querySelector(".contacts-button");
+
+var popup = document.querySelector(".write-us");
+
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector(".modal-form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("username");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  username.focus();
+
+  if (storage) {
+      username.value = storage;
+      email.focus();
+    } else {
+      username.focus();
+    }
+  });
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+form.addEventListener("submit", function (evt) {
+    if (!username.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+    	if (isStorageSupport) {
+        localStorage.setItem("username", username.value);
+      }
+    }
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
+
+
+  var mapLink = document.querySelector(".contacts-button-map");
+
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
